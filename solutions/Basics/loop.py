2363dac86fa091314eb201987d9f82a479165d43
Ravi Tambade <ravi.tambade@transflower.in>
2024-11-05 13:06:19 +0530

    catalog module

---
+++ solutions/Basics/loop.py
@@ -0,0 +1,18 @@
+count=78        #
+status=1        # boolean
+ch='T'          # character
+name="Transflower"  #string
+price=67.5
+"""
+#array of strings
+names=["Aarush","Ojas", "Archis","Sameer","Seeta"]
+print( name)
+for n in names:
+    print(n)
+    print("***")
+    print("Rama Aparment Resident")
+"""
+
+marks=[78,87,54,99] # array of integers
+for mark in marks:
+    print(mark)
